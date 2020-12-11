6b77932f1ecc40dd40b04b5bd1a032df53568a96
tyler-oreskey <toreskey@gmail.com>
2020-12-11 14:25:29 -0700

    added all domains for cors

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ const bodyParser = require('body-parser');
 const config = require('./src/config');
 const { handleError } = require('./src/helpers/error');
 
+const allowedOrigins = config.siteURLs.split(',').map((siteURL) => {
+  if (siteURL.includes('localhost')) {
+    return `http://${siteURL}`;
+  }
+  return `https://${siteURL}`;
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors({ origin: config.siteURL }));
-app.get('/health', (req, res) => res.send("OK!"))
+app.use(cors({ origin: allowedOrigins }));
+app.get('/health', (req, res) => res.send('OK!'))
 app.use(require('./src/controllers')(router));
 app.use((err, req, res, next) => handleError(err, res));
 
